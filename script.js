8ea881f0b18f6da9ae4f718a736324c55b5d2302
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-01-21 22:24:41 +0300

    docs: rewrite some code fix bug

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 //---------=-slider------------------------------------------
-const arrowLeft = document.querySelector('.slider__arrow--left'),
-  arrowRight = document.querySelector('.slider__arrow--right'),
-  sliderLine = document.querySelector('.background-phone'),
-  images = document.querySelectorAll('.slider-image'),
-  button = document.querySelectorAll('.tag'),
-  bckground = document.querySelector('.slider'),
-  image1 = document.querySelector('.slider-image1'),
-  image2 = document.querySelector('slider-image2'),
-  arrImg = Array.from(document.getElementsByClassName('image'));
+const arrowLeft = document.querySelector(".slider__arrow--left"),
+  arrowRight = document.querySelector(".slider__arrow--right"),
+  sliderLine = document.querySelector(".background-phone"),
+  images = document.querySelectorAll(".slider-image"),
+  button = document.querySelectorAll(".tag"),
+  bckground = document.querySelector(".slider"),
+  image1 = document.querySelector(".slider-image1"),
+  image2 = document.querySelector("slider-image2"),
+  arrImg = Array.from(document.getElementsByClassName("image"));
 
 let counter = 0;
-images[counter].style.display = 'block';
-arrowRight.addEventListener('click', () => {
+images[counter].style.display = "block";
+arrowRight.addEventListener("click", () => {
   let nextIndex = counter + 1;
   bckground.style.cssText = `background-color: #648BF0`;
   slide1.style.cssText = `visibility: hidden`;
@@ -21,23 +21,21 @@ arrowRight.addEventListener('click', () => {
     bckground.style.cssText = `background-color: #F06C64`;
     slide1.style.cssText = `visibility: visible`;
     slide2.style.cssText = `visibility: hidden`;
-  };
+  }
 
   const next = images[nextIndex];
   sliderLine.append(next);
-  next.style.display = 'block';
-  console.log('движение');
-  next.style.animation = 'moveRight 0.5s';
-
+  next.style.display = "block";
+  // console.log('движение');
+  next.style.animation = "moveRight 0.5s";
 
   counter += 1;
   if (counter >= images.length) {
     counter = 0;
-
-  };
+  }
 });
 
-arrowLeft.addEventListener('click', () => {
+arrowLeft.addEventListener("click", () => {
   let prevIndex = counter - 1;
   bckground.style.cssText = `background-color: #F06C64`;
   slide2.style.cssText = `visibility: hidden`;
@@ -51,14 +49,13 @@ arrowLeft.addEventListener('click', () => {
 
   const prev = images[prevIndex];
   sliderLine.append(prev);
-  prev.style.display = 'block';
+  prev.style.display = "block";
   // console.log('движение');
-  prev.style.animation = 'moveLeft 0.5s';
+  prev.style.animation = "moveLeft 0.5s";
 
   counter -= 1;
   if (counter < 0) {
     counter = images.length - 1;
-
   }
 });
 //--------------------Portfolio----------------------------------------------
@@ -66,9 +63,9 @@ const mixRand = (a, b) => Math.random() - 0.5;
 
 for (let i = 0; i < button.length; i++) {
   let but = button[i];
-  but.addEventListener('click', function (e) {
+  but.addEventListener("click", function (e) {
     // console.log('нажатие на кнопку перемешивания картинок' );
-    arrImgSrcMix = arrImg.map(e => e.src).sort(mixRand);
-    arrImg.map((e, i) => e.src = arrImgSrcMix[i]);
+    arrImgSrcMix = arrImg.map((e) => e.src).sort(mixRand);
+    arrImg.map((e, i) => (e.src = arrImgSrcMix[i]));
   });
-};
+}
