f16254aa80619495f173f61af7bc82d00043d80f
Uladzimir Prybylouski <eibiologos@gmail.com>
2021-03-01 20:34:34 +0300

    feat: add mouse click

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// const button = document.querySelectorAll(`div`);
+
 function removeTransition(e) {
   if (e.propertyName !== 'transform') return;
   e.target.classList.remove('playing');
@@ -6,6 +8,7 @@ function removeTransition(e) {
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+ 
   if (!audio) return;
 
   key.classList.add('playing');
@@ -16,3 +19,15 @@ function playSound(e) {
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
+window.addEventListener('click', playClick);
+
+
+function playClick(e) {
+  const mouseClick = e.target.closest(".key").getAttribute("data-key");
+  const audio = document.querySelector(`audio[data-key="${mouseClick}"]`);
+  const key = document.querySelector(`div[data-key="${mouseClick}"]`);
+ 
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+}
